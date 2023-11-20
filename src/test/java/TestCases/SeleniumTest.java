03027257096ffafdb55f77131d0a636880169fb2
siva2198 <msivaraman1998@gmail.com>
2023-11-20 18:00:11 +0530

    Changes

--- src/test/java/TestCases/SeleniumTest.java
+++ src/test/java/TestCases/SeleniumTest.java
@@ -8,13 +8,11 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
-
 public class SeleniumTest {
     public static WebDriver driver;
     static ExtentReports reports;
     public static ExtentTest test;
     static ExtentReports extent = new ExtentReports();
-
     @BeforeSuite
     public static void Setup() throws InterruptedException {
         driver = new ChromeDriver();
@@ -22,9 +20,7 @@ public class SeleniumTest {
         ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
         extent.attachReporter(spark);
         HomePage.click_hamburger_menu();
-        HomePage.click_online_product_link();
     }
-
     @Test
     void validateTitles_online_products() throws InterruptedException {
         test = extent.createTest("Validate Titles on Online page", "This test validates that all the titles is present in the online product page");
@@ -33,26 +29,22 @@ public class SeleniumTest {
         ProductPage.sneakers_getTitle_validate();
         extent.flush();
     }
-
     @Test
-    void validateFirstFormalShoe() {
+    void validateFirstFormalShoe() throws InterruptedException {
+        HomePage.click_online_product_link();
         test = extent.createTest("Validate First Formal Shoe Product title", "This test validates that all the titles is present in the formal shoe product");
         ProductPage.first_formalshoe_name_getTitle_validate();
     }
-
     @Test
     void validateFirstSportsShoe() throws InterruptedException {
         test = extent.createTest("Validate First Sports Shoe Product title", "This test validates that all the titles is present in the sports shoe product");
-
         ProductPage.first_sports_shoe_name_getTitle_validate();
     }
-
     @Test
     void validateFirstSneakerShoe() throws InterruptedException {
         test = extent.createTest("Validate First Sneaker Product title", "This test validates that all the titles is present in the sneakers product");
         ProductPage.first_sneakers_name_getTitle_validate();
     }
-
     @AfterSuite
     public static void cleanup() {
         extent.flush();
