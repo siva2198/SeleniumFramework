396df61cf32b10d818ae3cae335a3a4d57487a70
siva2198 <msivaraman1998@gmail.com>
2023-11-20 18:02:57 +0530

    Forgot to add

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -7,6 +7,7 @@ import static TestCases.SeleniumTest.driver;
 public class HomePage {
     public static String hamburger_menu_xpath = "//*[@id=\"menuToggle\"]/input";
     public static String online_product_link_xpath = "/html/body/div/nav/div/ul/a[3]"; // /html/body/div/nav/div/ul/a[3]/li
+    public static String SignInPortalHomepageMenu_xpath ="/html/body/div/nav/div/ul/a[2]";
 
     public static void click_hamburger_menu() {
         driver.findElement(By.xpath(hamburger_menu_xpath)).click();
@@ -15,5 +16,9 @@ public class HomePage {
     public static void click_online_product_link() throws InterruptedException {
         Thread.sleep(2000);
         driver.findElement(By.xpath(online_product_link_xpath)).click();
+    }
+    public static void click_signin_portal_menu_link() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.findElement(By.xpath(SignInPortalHomepageMenu_xpath)).click();
     }
 }
