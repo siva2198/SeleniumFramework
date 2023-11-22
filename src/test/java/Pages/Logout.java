fb0aa545145bbf9735a256e5e5f183a1ae38601d
siva2198 <msivaraman1998@gmail.com>
2023-11-22 17:52:32 +0530

    comment

--- src/test/java/Pages/Logout.java
+++ src/test/java/Pages/Logout.java
@@ -6,6 +6,7 @@ public class Logout{
     public static void logout(){
         driver.quit();
 // this will close the entire browser
+        //checkout this master and check whether it is updating or not
     }
 }
 
