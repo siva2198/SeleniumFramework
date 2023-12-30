cbdba55f14d4c372c98737547dbbbd6813118cf0
siva2198 <msivaraman1998@gmail.com>
2023-12-30 21:45:19 +0530

    start new project with framework

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -1,24 +1,5 @@
 package Pages;
 
-import org.openqa.selenium.By;
-
-import static TestCases.SeleniumTest.driver;
-
 public class HomePage {
-    public static String hamburger_menu_xpath = "//*[@id=\"menuToggle\"]/input";
-    public static String online_product_link_xpath = "/html/body/div/nav/div/ul/a[3]"; // /html/body/div/nav/div/ul/a[3]/li
-    public static String SignInPortalHomepageMenu_xpath ="/html/body/div/nav/div/ul/a[2]";
 
-    public static void click_hamburger_menu() {
-        driver.findElement(By.xpath(hamburger_menu_xpath)).click();
-    }
-
-    public static void click_online_product_link() throws InterruptedException {
-        Thread.sleep(2000);
-        driver.findElement(By.xpath(online_product_link_xpath)).click();
-    }
-    public static void click_signin_portal_menu_link() throws InterruptedException {
-        Thread.sleep(2000);
-        driver.findElement(By.xpath(SignInPortalHomepageMenu_xpath)).click();
-    }
 }
